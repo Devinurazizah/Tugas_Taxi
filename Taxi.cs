0f5a7053583f0dd5292ba50cac0e7281466a4744
Devi Nur Azizah <devinurazizah27gmail.com>
2021-03-17 15:37:25 +0700

    project kedua

---
+++ Taxi.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    class Taxi
+    {
+        //propertis
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        //method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("DriverName : {0}", DriverName);
+            Console.WriteLine("OnDuty : {0}", OnDuty ? "Yes":"No");
+            Console.WriteLine("NumPassenger : {0}", NumPassenger);
+        }
+
+        //
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("\n {0} Sedang Menjemput Penumpang", DriverName);
+        }
+
+        //
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} Selesai Mengantar Penumpang\n", DriverName);
+        }
+    }
+}
